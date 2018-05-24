9a24e0d8530c81c04002fe9a62af846f39526f58
AuriCA32 <auri.git.17@gmail.com>
2018-05-24 19:18:00 -0400

    Modificado el default template para mostrar todas las tablas
    Ahora se listan todas las tablas y pueden ademas listarse sus elementos

--- ProyectoSoftware/InscripcionPostgrado/urls.py
+++ ProyectoSoftware/InscripcionPostgrado/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
+import re
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name="index"), # Ruta del index
+    path('index', views.index, name="index"), # Ruta del index
+    re_path(r'/[a-zA-Z]*', views.index, name="index"),
 ]
--- ProyectoSoftware/InscripcionPostgrado/views.py
+++ ProyectoSoftware/InscripcionPostgrado/views.py
@@ -1,10 +1,60 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.db import connection
+from django.apps import apps
 from .models import *
+
+show_all_tables = True
+element_actual = None
 
 # Create your views here.
 def index(request):
-	context = {'table_name' : "Estudiante",
-		'table_column_list' : Estudiante.__getallfieldNames__(),
-		'table' : Estudiante.objects.all()
-	}
-	return render(request, 'crud/index.html', context)
+	if request.method=="POST":
+		#IMPLEMENTAR
+		try:
+			name=request.path.split("/")[1]
+			context=_buildContext__(name,True)
+			parameters = { }
+			for i in context["column_list"]:
+				parameters[i]=request.POST.get(i)
+			__modififyDB__(context["table"], parameters)
+			return  HttpResponseRedirect(name)
+		except:
+			print("No se pudo modificar la BD")
+			return  HttpResponseRedirect('')
+	else:
+		if request.path!="" and request.path!="/" and "index" not in request.path:
+			show_all_tables = False
+			name=request.path.split("/")[1]
+			return render(request, 'crud/index.html', __buildContext__(name,True))
+		else:
+			show_all_tables=True
+			return render(request, 'crud/index.html', __buildContext__("All tables",False))
+
+def __getDBList__():
+	all_tables = connection.introspection.table_names()
+	tables_to_use = []
+	for i in all_tables:
+		if "InscripcionPostgrado" in i:
+			tables_to_use.append(i.split("_",1)[1])
+	return tables_to_use
+
+def __buildContext__(name,ismodel):
+	if ismodel:
+		model = apps.get_model(app_label='InscripcionPostgrado', model_name=name)
+		column_list=model.__getallfieldNames__()
+		table=model.objects.all()
+	else:
+		column_list=["Nombre de la tabla"]
+		table=__getDBList__()
+	context = {
+			'table_name' : name,
+			'table_column_list' : column_list,
+			'table' : table,
+			'show_all_tables' : show_all_tables
+		}
+	return context
+
+def __modififyDB__(table, parameters):
+	element=table.__createElement__(parameters)
+	element.save()
