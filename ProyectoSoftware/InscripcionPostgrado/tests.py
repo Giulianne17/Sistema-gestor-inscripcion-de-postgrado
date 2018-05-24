037fef8c1e430c3367785cc3985ed518b2b85734
manuelinho96 <mr.1311223@gmail.com>
2018-05-24 12:59:56 -0400

    Agregado caso de prueba para agregar una coordinacion a la BD

--- ProyectoSoftware/InscripcionPostgrado/tests.py
+++ ProyectoSoftware/InscripcionPostgrado/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
 # Create your tests here.
+from InscripcionPostgrado.models import Coordinacion
+
+class CoordinacionTestCase(TestCase):
+    def setUp(self):
+        Coordinacion.objects.create(Cod_coordinacion = 3, Nombre_coordinacion = "Computacion")
+        Coordinacion.objects.create(Cod_coordinacion = 4, Nombre_coordinacion = "Arquitectura")
+    
+    def test_coordinacion_crear(self):
+        coord1 = Coordinacion.objects.get(Cod_coordinacion = 3)
+        coord2 = Coordinacion.objects.get(Nombre_coordinacion = "Arquitectura")
+        self.assertEqual(coord1.Nombre_coordinacion, "Computacion")
+        self.assertEqual(coord2.Cod_coordinacion, str(4))
