53f5a795709faa9ab1f1d636456cdb34591c2562
iankz <14-10406@usb.ve>
2018-05-29 16:40:00 -0400

    Agregados casos los de prueba para la tabla Asignatura

--- ProyectoSoftware/InscripcionPostgrado/tests.py
+++ ProyectoSoftware/InscripcionPostgrado/tests.py
@@ -7,45 +7,140 @@ from InscripcionPostgrado.forms import *
 
 # Pruebas de la tabla Coordinaciones
 # Caso de prueba para verificar que se añaden bien a la BD las coordinaciones
-class CoordinacionTestCase(TestCase):
-    def setUp(self):
-        pass
-    
-    def test_coordinacion_crear(self):
-        form_data = {
-            'Cod_coordinacion': "4", 
-            'Nombre_coordinacion': "Arquitectura"
-        }
-        form = CoordinacionForm(data=form_data)
-        form.save()
-        coord1 = Coordinacion.objects.get(Cod_coordinacion = 4)
-        self.assertEqual(coord1.Nombre_coordinacion, "Arquitectura")
-
-# Pruebas de la tabla Asignatura
 class AsignaturaTestCase(TestCase):
     def setUp(self):
-        coord = Coordinacion.objects.create(Cod_coordinacion = "4", Nombre_coordinacion = "Arquitectura")
+        coord = Coordinacion.objects.create(Cod_coordinacion = "EE", Nombre_coordinacion = "Arquitectura")
 
     # Verificar que se añade una asignatura
     def test_asignatura_crear(self):
         form_data = {
-            'Cod_asignatura': 'EE-102',
+            'Cod_asignatura': 'EE1020',
             'Nombre_asig': 'Estudios generales',
-            'Cod_coordinacion': '4',
+            'Cod_coordinacion': 'EE',
             'Creditos': '4'
         }
         form = AsignaturaForm(data = form_data)
         form.save()
         asig = Asignatura.objects.get(Nombre_asig = "Estudios generales")
-        self.assertEqual(asig.Cod_asignatura, "EE-102")
+        self.assertEqual(asig.Cod_asignatura, 'EE1020')
 
+    # Verificar que se busca una asignatura
+    def test_asignatura_consulta(self):
+
+        form_data = {
+            'Cod_asignatura': 'MA1111',
+            'Nombre_asig': 'Matematicas I',
+            'Cod_coordinacion': 'EE',
+            'Creditos': '4'
+        }
+        form = AsignaturaForm(data = form_data)
+        form.save()
+        asig = Asignatura.objects.filter(Nombre_asig = "Matematicas I")
+        self.assertEqual(asig[0].Cod_asignatura, 'MA1111')
+
+    # Verificar que se elimina una asignatura
+    def test_asignatura_eliminar(self):
+        form_data = {
+            'Cod_asignatura': 'EE1020',
+            'Nombre_asig': 'Estudios generales',
+            'Cod_coordinacion': 'EE',
+            'Creditos': '4'
+        }
+        form = AsignaturaForm(data = form_data)
+        form.save()
+        asig = Asignatura.objects.get(Nombre_asig = "Estudios generales").delete()
+        try:
+            asig1 = Asignatura.objects.get(Nombre_asig = "Estudios generales")
+        except:
+            pass
     # Verificar que no se añade asignatura con una cantidad de creditos mayores a 30
+
     def test_asignatura_maxcredit(self):
         form_data = {
-            'Cod_asignatura': 'EE-105',
+            'Cod_asignatura': 'EE1050',
             'Nombre_asig': 'Estudios Generales',
-            'Cod_coordinacion': '4',
+            'Cod_coordinacion': 'EE',
             'Creditos': '50'
         }
         form = AsignaturaForm(data=form_data)
         self.assertFalse(form.is_valid())
+
+    # Verificar que no se añade asignatura con longitud mayor a 30 en el nombre
+
+    def test_asignatura_maxnombre(self):
+        form_data = {
+            'Cod_asignatura': 'EE1050',
+            'Nombre_asig': 'Estudios GeneralesEstudios Generales',
+            'Cod_coordinacion': 'EE',
+            'Creditos': '3'
+        }
+        form = AsignaturaForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    # Verificar que no se añade asignatura con longitud mayor a 6 en el codigo
+
+    def test_asignatura_maxcod(self):
+        form_data = {
+            'Cod_asignatura': 'EE00555',
+            'Nombre_asig': 'Estudios Generales',
+            'Cod_coordinacion': 'EE',
+            'Creditos': '3'
+        }
+        form = AsignaturaForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    # Verificar que no se añade asignatura con longitud menor a 6 en el codigo
+
+    def test_asignatura_mincod(self):
+        form_data = {
+            'Cod_asignatura': 'EE005',
+            'Nombre_asig': 'Estudios Generales',
+            'Cod_coordinacion': 'EE',
+            'Creditos': '3'
+        }
+        form = AsignaturaForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    # Verificar que se añade una asignatura con la longitud maxima para el nombre
+
+    def test_asignatura_exactnombre(self):
+        form_data = {
+            'Cod_asignatura': 'EE0050',
+            'Nombre_asig': 'Estudios GeneralesEstudios Gen',
+            'Cod_coordinacion': 'EE',
+            'Creditos': '3'
+        }
+        form = AsignaturaForm(data=form_data)
+        form.save()
+        asig1 = Asignatura.objects.get(Nombre_asig = 'Estudios GeneralesEstudios Gen')
+        self.assertEqual(asig1.Cod_asignatura, 'EE0050')
+
+    # Verificar que se añade una asignatura con la longitud maxima para el codigo
+
+    def test_asignatura_exactcod(self):
+        form_data = {
+            'Cod_asignatura': 'EE1050',
+            'Nombre_asig': 'Estudios Generales',
+            'Cod_coordinacion': 'EE',
+            'Creditos': '3'
+        }
+        form = AsignaturaForm(data=form_data)
+        form.save()
+        asig1 = Asignatura.objects.get(Nombre_asig = 'Estudios Generales')
+        self.assertEqual(asig1.Cod_asignatura, 'EE1050')
+
+    # Verificar que se añade una asignatura con la cantidad maxima para los creditos
+
+    def test_asignatura_exactcred(self):
+        form_data = {
+            'Cod_asignatura': 'EE1050',
+            'Nombre_asig': 'Estudios Generales',
+            'Cod_coordinacion': 'EE',
+            'Creditos': '30'
+        }
+        form = AsignaturaForm(data=form_data)
+        form.save()
+        asig1 = Asignatura.objects.get(Nombre_asig = 'Estudios Generales')
+        self.assertEqual(asig1.Cod_asignatura, 'EE1050')
+
+
