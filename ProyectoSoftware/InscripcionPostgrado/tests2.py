8daf24401511cb9c84078f8f2b904aead1d08362
Giulianne17 <gvtr96@gmail.com>
2018-06-13 16:33:49 -0400

    Agregado casos de prueba para la tabla Se_Ofrece Se evaluan los casos frontera y esquina del campo horario. Se evaluan casos esquina y frontera del campo anio. Se evaluan los casos de esquina del campo periodo.

--- ProyectoSoftware/InscripcionPostgrado/tests2.py
+++ ProyectoSoftware/InscripcionPostgrado/tests2.py
@@ -153,3 +153,254 @@ class ProfesorTestCase(TestCase):
 		form = ProfesorForm(data = form_data)
 		form.save()
 		self.assertTrue(form.is_valid())
+
+
+# Pruebas de la tabla Ofrece
+class OfreceTestCase(TestCase):
+	def setUp(self):
+		coord = Coordinacion.objects.create(Cod_coordinacion = "EE", Nombre_coordinacion = "Arquitectura")
+
+		form_data = {
+			'Cod_asignatura': 'EE-1020',
+			'Nombre_asig': 'Estudios generales',
+			'Cod_coordinacion': 'EE',
+			'Creditos': '4',
+			'Programa': 'https://www.youtube.com/watch?v=C6MOKXm8x50'
+		}
+		form = AsignaturaForm(data = form_data)
+		form.save()
+
+		form_data1 = {
+			'Id_prof' : '24553623',
+			'Apellidos' : 'Aular Lopez',
+			'Nombres' : 'Luis Jose',
+			'Cod_coordinacion': 'EE'
+		}
+		form1 = ProfesorForm(data = form_data1)
+		form1.save()
+
+	# Caso esquina.
+	def test_ofrece_horarioEsquina1(self):
+		form_data = {
+			'Id_prof' : '24553623',
+			'Cod_asignatura' : 'EE-1020',
+			'Horario' : '1-2',
+			'Dia' : 'LUNES',
+			'Periodo' : 'EM',
+			'Anio': 2018,
+			'Cod_coordinacion': 'EE'
+		}
+		form = Se_OfreceForm(data = form_data)
+		form.save()
+		self.assertTrue(form.is_valid())
+
+	def test_ofrece_horarioEsquina2(self):
+		form_data = {
+			'Id_prof' : '24553623',
+			'Cod_asignatura' : 'EE-1020',
+			'Horario' : '1-13',
+			'Dia' : 'LUNES',
+			'Periodo' : 'EM',
+			'Anio': 2018,
+			'Cod_coordinacion': 'EE'
+		}
+		form = Se_OfreceForm(data = form_data)
+		form.save()
+		self.assertTrue(form.is_valid())
+
+	def test_ofrece_horarioEsquina3(self):
+		form_data = {
+			'Id_prof' : '24553623',
+			'Cod_asignatura' : 'EE-1020',
+			'Horario' : '12-13',
+			'Dia' : 'LUNES',
+			'Periodo' : 'EM',
+			'Anio': 2018,
+			'Cod_coordinacion': 'EE'
+		}
+		form = Se_OfreceForm(data = form_data)
+		form.save()
+		self.assertTrue(form.is_valid())
+
+	# Casos frontera.
+	def test_ofrece_horarioFrontera1(self):
+		form_data = {
+			'Id_prof' : '24553623',
+			'Cod_asignatura' : 'EE-1020',
+			'Horario' : '1-5',
+			'Dia' : 'LUNES',
+			'Periodo' : 'AJ',
+			'Anio': 2018,
+			'Cod_coordinacion': 'EE'
+		}
+		form = Se_OfreceForm(data = form_data)
+		form.save()
+		self.assertTrue(form.is_valid())
+
+	def test_ofrece_horarioFrontera2(self):
+		form_data = {
+			'Id_prof' : '24553623',
+			'Cod_asignatura' : 'EE-1020',
+			'Horario' : '7-13',
+			'Dia' : 'LUNES',
+			'Periodo' : 'EM',
+			'Anio': '2018',
+			'Cod_coordinacion': 'EE'
+		}
+		form = Se_OfreceForm(data = form_data)
+		form.save()
+		self.assertTrue(form.is_valid())
+
+	def test_ofrece_horarioFrontera3(self):
+		form_data = {
+			'Id_prof' : '24553623',
+			'Cod_asignatura' : 'EE-1020',
+			'Horario' : '7-8',
+			'Dia' : 'LUNES',
+			'Periodo' : 'V',
+			'Anio': 2018,
+			'Cod_coordinacion': 'EE'
+		}
+		form = Se_OfreceForm(data = form_data)
+		form.save()
+		self.assertTrue(form.is_valid())
+
+	def test_ofrece_horarioFrontera4(self):
+		form_data = {
+			'Id_prof' : '24553623',
+			'Cod_asignatura' : 'EE-1020',
+			'Horario' : '5-6',
+			'Dia' : 'LUNES',
+			'Periodo' : 'EM',
+			'Anio': 2018,
+			'Cod_coordinacion': 'EE'
+		}
+		form = Se_OfreceForm(data = form_data)
+		form.save()
+		self.assertTrue(form.is_valid())
+
+	def test_ofrece_anioEsquina1(self):
+		form_data = {
+			'Id_prof' : '24553623',
+			'Cod_asignatura' : 'EE-1020',
+			'Horario' : '5-6',
+			'Dia' : 'LUNES',
+			'Periodo' : 'SD',
+			'Anio': 1970,
+			'Cod_coordinacion': 'EE'
+		}
+		form = Se_OfreceForm(data = form_data)
+		form.save()
+		self.assertTrue(form.is_valid())
+
+	def test_ofrece_anioEsquina2(self):
+		form_data = {
+			'Id_prof' : '24553623',
+			'Cod_asignatura' : 'EE-1020',
+			'Horario' : '5-6',
+			'Dia' : 'LUNES',
+			'Periodo' : 'SD',
+			'Anio': 2019,
+			'Cod_coordinacion': 'EE'
+		}
+		form = Se_OfreceForm(data = form_data)
+		form.save()
+		self.assertTrue(form.is_valid())
+
+	def test_ofrece_anioFrontera1(self):
+		form_data = {
+			'Id_prof' : '24553623',
+			'Cod_asignatura' : 'EE-1020',
+			'Horario' : '5-6',
+			'Dia' : 'LUNES',
+			'Periodo' : 'SD',
+			'Anio': 2000,
+			'Cod_coordinacion': 'EE'
+		}
+		form = Se_OfreceForm(data = form_data)
+		form.save()
+		self.assertTrue(form.is_valid())
+
+	def test_ofrece_anioFrontera2(self):
+		form_data = {
+			'Id_prof' : '24553623',
+			'Cod_asignatura' : 'EE-1020',
+			'Horario' : '5-6',
+			'Dia' : 'LUNES',
+			'Periodo' : 'SD',
+			'Anio': 1990,
+			'Cod_coordinacion': 'EE'
+		}
+		form = Se_OfreceForm(data = form_data)
+		form.save()
+		self.assertTrue(form.is_valid())
+
+	def test_ofrece_anioFrontera3(self):
+		form_data = {
+			'Id_prof' : '24553623',
+			'Cod_asignatura' : 'EE-1020',
+			'Horario' : '5-6',
+			'Dia' : 'LUNES',
+			'Periodo' : 'SD',
+			'Anio': 2018,
+			'Cod_coordinacion': 'EE'
+		}
+		form = Se_OfreceForm(data = form_data)
+		form.save()
+		self.assertTrue(form.is_valid())
+
+	def test_ofrece_periodoEsquina1(self):
+		form_data = {
+			'Id_prof' : '24553623',
+			'Cod_asignatura' : 'EE-1020',
+			'Horario' : '5-6',
+			'Dia' : 'LUNES',
+			'Periodo' : 'SD',
+			'Anio': 2018,
+			'Cod_coordinacion': 'EE'
+		}
+		form = Se_OfreceForm(data = form_data)
+		form.save()
+		self.assertTrue(form.is_valid())
+
+	def test_ofrece_periodoEsquina2(self):
+		form_data = {
+			'Id_prof' : '24553623',
+			'Cod_asignatura' : 'EE-1020',
+			'Horario' : '5-6',
+			'Dia' : 'LUNES',
+			'Periodo' : 'EM',
+			'Anio': 2018,
+			'Cod_coordinacion': 'EE'
+		}
+		form = Se_OfreceForm(data = form_data)
+		form.save()
+		self.assertTrue(form.is_valid())
+
+	def test_ofrece_periodoEsquina3(self):
+		form_data = {
+			'Id_prof' : '24553623',
+			'Cod_asignatura' : 'EE-1020',
+			'Horario' : '5-6',
+			'Dia' : 'LUNES',
+			'Periodo' : 'AJ',
+			'Anio': 2018,
+			'Cod_coordinacion': 'EE'
+		}
+		form = Se_OfreceForm(data = form_data)
+		form.save()
+		self.assertTrue(form.is_valid())
+
+	def test_ofrece_periodoEsquina4(self):
+		form_data = {
+			'Id_prof' : '24553623',
+			'Cod_asignatura' : 'EE-1020',
+			'Horario' : '5-6',
+			'Dia' : 'LUNES',
+			'Periodo' : 'V',
+			'Anio': 2018,
+			'Cod_coordinacion': 'EE'
+		}
+		form = Se_OfreceForm(data = form_data)
+		form.save()
+		self.assertTrue(form.is_valid())
