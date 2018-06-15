8c4169e7012c728127498e98f89a58e083cfa106
sandra <sandrav0609@gmail.com>
2018-06-15 09:04:55 -0400

    Agregados comentarios para forms.py

--- ProyectoSoftware/InscripcionPostgrado/forms.py
+++ ProyectoSoftware/InscripcionPostgrado/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.forms import ModelForm
 from InscripcionPostgrado.models import *
 
-# Form para añadir una asignatura
+# Form para añadir una instancia a la tabla de asignaturas
 class AsignaturaForm(ModelForm):
     class Meta:
         model = Asignatura
@@ -13,79 +13,81 @@ class AsignaturaForm(ModelForm):
             }
         }
 
-# Form para añadir una coordinacion
+# Form para añadir una instancia a la tabla de las coordinaciones
 class CoordinacionForm(ModelForm):
     class Meta:
         model = Coordinacion
         fields = ["Cod_coordinacion", "Nombre_coordinacion"]
 
-# Form para añadir un profesor
+# Form para añadir una instancia a la tabla de profesor
 class ProfesorForm(ModelForm):
     class Meta:
         model = Profesor
         fields = ["Id_prof","Apellidos","Nombres","Cod_coordinacion"]
 
-# Form para añadir un pertenece
+# Form para añadir una instancia a la tabla de pertenece que relaciona un decanato con una coordinacion
 class PerteneceForm(ModelForm):
     class Meta:
         model = Pertenece
         fields = ["Nombre_decanato","Cod_coordinacion"]
 
-# Form para añadir un decanato
+# Form para añadir una instancia a la tabla de decanato
 class DecanatoForm(ModelForm):
     class Meta:
         model = Decanato
         fields = ["Nombre_decanato"]
 
-# Form para añadir un estudiante
+# Form para añadir una instancia la tabla de estudiante
 class EstudianteForm(ModelForm):
     class Meta:
         model = Estudiante
         fields = ["Carnet","Apellidos","Nombres"]
 
-# Form para añadir un cursa
+# Form para añadir una instancia a la tabla de cursa que relaciona un estudiante con una asignatura que
+#cursa, ofertada en cierto periodo y año
 class CursaForm(ModelForm):
     class Meta:
         model = Cursa
         fields = ["Carnet","Cod_asignatura","Periodo","Anio"]
 
-# Form para añadir un se_ofrece
+# Form para añadir una instancia a la tabla de se_ofrece, que relaciona un profesor con un una asignatura
+#que se ofrece en cierto período y año, con un horario y día en que se imparte la materia
 class Se_OfreceForm(ModelForm):
     class Meta:
         model = Se_Ofrece
         fields = ["Id_prof","Cod_asignatura","Horario","Dia","Periodo","Anio", "Cod_coordinacion"]
 
-# Form para añadir un MedioPago
+# Form para añadir una instancia a la tabla de MedioPago
 class MedioPagoForm(ModelForm):
     class Meta:
         model = MedioPago
         fields = ["Postiza"]
 
-# Form para añadir un MedioPago
+# Form para añadir una instancia a la tabla de Paga_Con
 class Paga_ConForm(ModelForm):
     class Meta:
         model = Paga_Con
         fields = ["Precio","Carnet","Postiza","Periodo","Anio"]
 
-# Form para añadir un Debito
+# Form para añadir una instancia a la tabla de tarjeta de Debito
 class DebitoForm(ModelForm):
     class Meta:
         model = Debito
         fields = ["Nro_Cuenta","Nro_Tarjeta","Tipo","Nombre_Banco","Postiza"]
 
-# Form para añadir un Credito
+# Form para añadir una instancia a la tabla de tarjeta de Credito
 class CreditoForm(ModelForm):
     class Meta:
         model = Credito
         fields = ["Nro_Tarjeta","Fecha_Vence","Nombre_Banco","Postiza"]
 
-# Form para añadir un Transferencia
+# Form para añadir una instancia a la tabla de Transferencia de pago
 class TransferenciaForm(ModelForm):
     class Meta:
         model = Transferencia
         fields = ["Nro_Referencia","Postiza"]
 
-# Form para añadir un Deposito
+# Form para añadir una instancia a la tabla de Deposito de pago
 class DepositoForm(ModelForm):
     class Meta:
         model = Deposito
